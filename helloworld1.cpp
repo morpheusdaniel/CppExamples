547c54f492f8c76e2a2f97ddc3fc3f00ed93bfb9
morpheusdaniel <202morpheus@gmail.com>
2016-10-06 10:42:24 +0900

    Create helloworld1.cpp

---
+++ helloworld1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(void)
+{
+  std::cout<< "Hello World!!" <<std::endl;
+  std::cout<< "Hello " << "World!!" <<std::endl;
+  std::cout<<1<< 'a' << "String" <<std::endl;
+  return 0;
+}
