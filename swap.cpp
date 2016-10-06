f382271081df1d9f624bb6117bc3a3ce11bc947a
morpheusdaniel <202morpheus@gmail.com>
2016-10-06 10:57:26 +0900

    Create swap.cpp

---
+++ swap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+void swap(int *a, int *b){
+	int temp;
+	temp = a; a = b; b = temp;
+}
+
+void swap(char *a, char *b){
+	char temp;
+	temp = a; a = b; b = temp;
+}
+
+void swap(double *a, double *b){
+	double temp;
+	temp = a; a = b; b = temp;
+}
+
+int main()
+{}
