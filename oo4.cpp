55b14a111f8b99f7e01e5396a3fcf098774fa225
morpheusdaniel <202morpheus@gmail.com>
2016-10-07 18:18:07 +0900

    Update oo4.cpp

--- oo4.cpp
+++ oo4.cpp
@@ -1,3 +1,30 @@
 #include <iostream>
 using std::cout;
-us
+using std::endl;
+
+class Account {
+public:
+  char accID[20];
+  char secID[20];
+  char name[20];
+  int balance;
+  
+  void Deposit(int money){
+    balance += money;
+  }
+  void Withdraw(int money){
+    balance -= money;
+  }
+};
+
+int main(void)
+{
+  Account yoon = {"1234", "2321", "yoon", 1000};
+  
+  yoon.Deposit(100);
+  cout<<"Exist:"<<yoon.balance<<endl;
+  
+  yoon.Withdraw(200);
+  cout<<"Exist:"<<yoon.balance<<endl;
+  return 0;
+}
